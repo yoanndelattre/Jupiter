ae909e26a82afe353945b6c0a8d8999cfaa9aad1
yoanndelattre <yoanndelattre21@gmail.com>
2023-10-19 19:28:08 +0200

    add proxy support

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/yoanndelattre/Jupiter
 
 go 1.18
+
+require golang.org/x/net v0.17.0 // indirect
--- main.go
+++ main.go
@@ -1,42 +1,69 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
-	"sync"
+	"net/url"
+	"time"
+
+	"golang.org/x/net/proxy"
 )
 
 func main() {
-	// Define the list of backend servers (replace with your own backend server URLs).
-	backendServers := []string{
-		"https://www.google.com",
+	targetURL := flag.String("target", "http://localhost:8080", "Target URL for the load balancer")
+	proxyAddr := flag.String("proxy", "", "SOCKS5 proxy address (e.g., 127.0.0.1:1080)")
+	requestsPerSecond := flag.Int("rps", 10, "Requests per second")
+	duration := flag.Duration("duration", 10*time.Second, "Test duration")
+	flag.Parse()
+
+	fmt.Printf("Testing Load Balancer with %d requests per second for %v\n", *requestsPerSecond, *duration)
+
+	var dialer proxy.Dialer
+	if *proxyAddr != "" {
+		// Initialize SOCKS5 proxy (if provided).
+		proxyURL, err := url.Parse("socks5://" + *proxyAddr)
+		if err != nil {
+			log.Fatalf("Error parsing proxy address: %v", err)
+		}
+		dialer, err = proxy.FromURL(proxyURL, proxy.Direct)
+		if err != nil {
+			log.Fatalf("Error creating proxy dialer: %v", err)
+		}
 	}
 
-	// Number of requests to send per client.
-	numRequestsPerClient := 10
-
-	// Number of concurrent clients.
-	numClients := 5
-
-	var wg sync.WaitGroup
-
-	for i := 0; i < numClients; i++ {
-		wg.Add(1)
-		go func(clientID int) {
-			defer wg.Done()
-
-			for j := 0; j < numRequestsPerClient; j++ {
-				backendURL := backendServers[j%len(backendServers)]
-				resp, err := http.Get(backendURL)
-				if err != nil {
-					log.Printf("Client %d: Error making request to %s: %v\n", clientID, backendURL, err)
-					continue
-				}
-				defer resp.Body.Close()
-				log.Printf("Client %d: Request to %s returned status code %d\n", clientID, backendURL, resp.StatusCode)
-			}
-		}(i)
+	// Create an HTTP client with the proxy dialer if it's set.
+	httpClient := &http.Client{
+		Transport: &http.Transport{
+			Dial: dialer.Dial,
+		},
 	}
 
-	wg.Wait()
+	// Start the test.
+	startTime := time.Now()
+	requestCount := 0
+	for {
+		select {
+		case <-time.After(time.Second / time.Duration(*requestsPerSecond)):
+			go func() {
+				sendRequest(httpClient, *targetURL)
+				requestCount++
+			}()
+		case <-time.After(*duration):
+			elapsedTime := time.Since(startTime)
+			fmt.Printf("Test completed. Sent %d requests in %v\n", requestCount, elapsedTime)
+			return
+		}
+	}
+}
+
+func sendRequest(client *http.Client, targetURL string) {
+	resp, err := client.Get(targetURL)
+	if err != nil {
+		log.Printf("Error sending request to %s: %v\n", targetURL, err)
+		return
+	}
+	defer resp.Body.Close()
+	fmt.Printf("Request to %s completed with status: %s\n", targetURL, resp.Status)
 }
