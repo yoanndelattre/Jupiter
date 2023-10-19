5fef8b4761bc5418ec3cf09f5164b7ca7f339a84
yoanndelattre <yoanndelattre21@gmail.com>
2023-10-19 19:56:58 +0200

    add optionnal proxy

--- main.go
+++ main.go
@@ -2,27 +2,21 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
 	"net/http"
 	"net/url"
-	"time"
 
 	"golang.org/x/net/proxy"
 )
 
 func main() {
-	targetURL := flag.String("target", "http://localhost:8080", "Target URL for the load balancer")
+	targetURL := flag.String("target", "https://www.google.com", "Target URL for the load balancer")
 	proxyAddr := flag.String("proxy", "", "SOCKS5 proxy address (e.g., 127.0.0.1:1080)")
-	requestsPerSecond := flag.Int("rps", 10, "Requests per second")
-	duration := flag.Duration("duration", 10*time.Second, "Test duration")
 	flag.Parse()
-
-	fmt.Printf("Testing Load Balancer with %d requests per second for %v\n", *requestsPerSecond, *duration)
 
 	var dialer proxy.Dialer
 	if *proxyAddr != "" {
-		// Initialize SOCKS5 proxy (if provided).
+		// Initialize SOCKS5 proxy dialer if proxy is set.
 		proxyURL, err := url.Parse("socks5://" + *proxyAddr)
 		if err != nil {
 			log.Fatalf("Error parsing proxy address: %v", err)
@@ -33,28 +27,20 @@ func main() {
 		}
 	}
 
-	// Create an HTTP client with the proxy dialer if it's set.
-	httpClient := &http.Client{
-		Transport: &http.Transport{
+	// Create an HTTP client with or without the proxy dialer.
+	httpClient := &http.Client{}
+	if dialer != nil {
+		httpClient.Transport = &http.Transport{
 			Dial: dialer.Dial,
-		},
+		}
+		log.Printf("The SOCKS5 proxy is set\n")
+	} else {
+		log.Printf("The SOCKS5 proxy is not set\n")
 	}
 
 	// Start the test.
-	startTime := time.Now()
-	requestCount := 0
 	for {
-		select {
-		case <-time.After(time.Second / time.Duration(*requestsPerSecond)):
-			go func() {
-				sendRequest(httpClient, *targetURL)
-				requestCount++
-			}()
-		case <-time.After(*duration):
-			elapsedTime := time.Since(startTime)
-			fmt.Printf("Test completed. Sent %d requests in %v\n", requestCount, elapsedTime)
-			return
-		}
+		sendRequest(httpClient, *targetURL)
 	}
 }
 
@@ -65,5 +51,5 @@ func sendRequest(client *http.Client, targetURL string) {
 		return
 	}
 	defer resp.Body.Close()
-	fmt.Printf("Request to %s completed with status: %s\n", targetURL, resp.Status)
+	log.Printf("Request to %s completed with status: %s\n", targetURL, resp.Status)
 }
